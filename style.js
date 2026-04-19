f1c26e43f8c4e8bb60a9f41ff6a5891ae705507c
DelfinaLoreley <delfiloreley@gmail.com>
2026-04-19 16:11:14 -0300

    cambios del maquetado modo oscuro
    Cambios: nombre del archivo css, nuevas imgs, las señales ahora tienen sombras (los botones no funcionan)

--- style.js
+++ style.js
@@ -1,13 +1,1 @@
 
-const toggle = document.getElementById("themeToggle");
-const body = document.body;
-
-body.classList.add("light");
-
-toggle.addEventListener("click", () => {
-  if (body.classList.contains("light")) {
-    body.classList.replace("light", "dark");
-  } else {
-    body.classList.replace("dark", "light");
-  }
-});
