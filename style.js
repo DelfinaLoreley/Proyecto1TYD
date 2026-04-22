ab49062b145bda947fd349290bd2cee2906b6e0d
Mateo-LR <mateolewelruiz200@gmail.com>
2026-04-22 17:37:26 -0300

    función de cambio de temas

--- style.js
+++ style.js
@@ -1,1 +1,16 @@
+const toggle = document.getElementById("themeToggle");
+const themeLink = document.getElementById("estilo");
+
+let temaActual = "claro"; // estado inicial
+
+toggle.addEventListener("click", () => {
+
+    if (temaActual === "claro") {
+        themeLink.href = "styles/ModoOscuro.css";
+        temaActual = "oscuro";
+    } else {
+        themeLink.href = "styles/ModoClaro.css";
+        temaActual = "claro";
+    }
+});
 
