6f1b94a72266de72b389785341edfeb1184423c3
ODICRACK <nahuel.odi.tv@gmail.com>
2026-04-22 17:58:22 -0300

    visivilidad del filtrado

--- scrips/Filtrado.js
+++ scrips/Filtrado.js
@@ -1,4 +1,4 @@
-function filtrado(cate){
+function filtrado(cate, element){
   const contenedor = document.getElementById("contenedorDeSeniales");
   const hijos = contenedor.children;
 
@@ -11,4 +11,29 @@ function filtrado(cate){
       hijo.style.display = "none"; // ocultar
     }
   }
+  cambio(cate, element)
+}
+function cambio(num,e){
+  const coso = document.getElementById("cosoblanco")
+  const img = e.querySelector("img");
+  const titulo = e.querySelector("h2");
+  const posiciones =[29,105,191,271,353,433,517]
+  const items = document.querySelectorAll(".item");
+
+  for (let i = 0; i <= 6; i++) {
+    if (i === num) continue;
+
+    const item = items[i];
+    if (!item) continue;
+
+    const img = item.querySelector("img");
+    const titulo = item.querySelector("h2");
+    if (img) {
+      img.src = `assets/sin${i}.png`;
+      titulo.style.color = "var(--fontColor)";
+    }
+  }
+  coso.style.transform = `translateY(${posiciones[num]}px)`;
+  img.src = `assets/color${num}.png`
+  titulo.style.color = "red";
 }
